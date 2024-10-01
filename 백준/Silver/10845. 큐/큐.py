5c9fcf19c93403172546407587c3c6345839e7c2
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-10-01 21:10:15 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+stack = deque([])
+for i in range(n):
+    lst = sys.stdin.readline().split()
+    if lst[0] == "push":
+        stack.appendleft(lst[1])
+    elif lst[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif lst[0] == "size":
+        print(len(stack))
+    elif lst[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+
+    elif lst[0] == "front":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+    elif lst[0] == "back":
+        if stack:
+            print(stack[0])
+        else:
+            print(-1)
+
