2cbbe7c31b6f6597b4af9f189ba89d232c49ec8d
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-10-21 18:54:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 28 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,34 @@
 import sys
-sys.setrecursionlimit(10000)
-
-
 
 def dfs(x,y):
     global cnt
     vis[x][y] = 1
-    cnt += 1
-    for nextX,nextY in chk:
-        X = x+nextX
-        Y = y+nextY
-        if (0 <= X < n) and (0 <= Y < n) and lst[X][Y] and (not vis[X][Y]):
 
-            dfs(X,Y)
+    for i in chk:
+        nextX = x+i[0]
+        nextY = y+i[1]
+        if (0<=nextX<n) and (0<=nextY<n) and not vis[nextX][nextY] and lst[nextX][nextY]:
+            cnt += 1
+
+            dfs(nextX,nextY)
+
 
 n = int(sys.stdin.readline())
 lst = []
 for i in range(n):
-    lst.append(list(map(int,sys.stdin.readline().strip())))
+    lst.append(list(map(int," ".join(sys.stdin.readline()).split())))
 cnt = 0
+cntlst = []
 vis = [[0 for _ in range(n)] for __ in range(n)]
 chk = [[1,0],[0,1],[-1,0],[0,-1]]
-cntLst = []
 for i in range(n):
     for j in range(n):
         if lst[i][j] and not vis[i][j]:
             dfs(i,j)
-            cntLst.append(cnt)
+            cntlst.append(cnt)
             cnt = 0
-print(len(cntLst))
-cntLst.sort()
-for i in cntLst:
-    print(i)
 
-
+print(len(cntlst))
+cntlst.sort()
+for i in cntlst:
+    print(i+1)
