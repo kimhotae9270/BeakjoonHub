cfd12ff90baffd3c7f22afdc2965d7298ff2d7d4
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-11-06 21:22:19 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 6500 ms, Memory: 302568 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,62 +1,44 @@
 import sys
 import heapq
+input = sys.stdin.readline
 
 
-def isEmpty(nums):
-    for item in nums:
-        if item[1] > 0:
-            return False
-    return True
+test = int(input())
+for _ in range(test):
+    max_heap, min_heap = [], []
+    visit = [False] * 1_000_001
 
+    order_num = int(input())
 
-t = int(sys.stdin.readline())
+    for key in range(order_num):
+        order = input().rsplit()
+        if order[0] == 'I':
+            heapq.heappush(min_heap, (int(order[1]), key))
+            heapq.heappush(max_heap, (int(order[1]) * -1, key))
+            visit[key] = True #True이면 어떤 힙에서도 아직 삭제되지 않은 상태
 
-for i in range(t):
-    min_heap = []
-    max_heap = []
-    nums = dict()
-    k = int(sys.stdin.readline())
+        elif order[0] == 'D':
+            if order[1] == '-1': #삭제연산시, key값을 기준으로 해당 노드가 다른힙에서 삭제된 노드인가를 먼저 판단한다.
+                # 이미 상대힙에 의해 삭제된 노드인경우 삭제되지 않은 노드가 나올때까지 계쏙 버리다가 이후 삭제대상노드가 나오면 삭제한다.
+                while min_heap and not visit[min_heap[0][1]]: # visit이 False일떄 -> 해당노드가 삭제된상태
+                    heapq.heappop(min_heap) # 버림 (상대힙에서 이미 삭제된노드이므로)
+                if min_heap:
+                    visit[min_heap[0][1]] = False # visit이 Ture엿으므로 False로 바꾸고 내가 삭제함
+                    heapq.heappop(min_heap)
+            elif order[1] == '1':
+                while max_heap and not visit[max_heap[0][1]]: #이미 삭제된 노드인경우 삭제되지 않은 노드가 나올때까지 모두 버린다.
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visit[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
 
-    for j in range(k):
-        oprt, oprd = sys.stdin.readline().split()
-        num = int(oprd)
+# 모든 연산이 끝난후에도 ㅅ쓰레기 노드가 들어있을수 있으므로, 결과를 내기전 모두 비우고 각 힙의 첫번째 원소값을 출력한다. 
+    while min_heap and not visit[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visit[max_heap[0][1]]:
+        heapq.heappop(max_heap)
 
-        if oprt == 'I':
-            # 중복 삽입일 때
-            if num in nums:
-                nums[num] += 1
-            # 처음 삽입일 때
-            else:
-                nums[num] = 1
-                # min_heap에 추가
-                heapq.heappush(min_heap, num)
-                # max_heap에 추가
-                heapq.heappush(max_heap, -num)
-
-        elif oprt == 'D':
-            # 큐가 비어있지 않을 때만
-            if not isEmpty(nums.items()):
-                # 최댓값을 제거
-                if num == 1:
-                    while -max_heap[0] not in nums or nums[-max_heap[0]] < 1:
-                        temp = -heapq.heappop(max_heap)
-                        if temp in nums:
-                            del (nums[temp])
-                    nums[-max_heap[0]] -= 1
-                # 최솟값을 제거
-                else:
-                    while min_heap[0] not in nums or nums[min_heap[0]] < 1:
-                        temp = heapq.heappop(min_heap)
-                        if temp in nums:
-                            del (nums[temp])
-                    nums[min_heap[0]] -= 1
-
-    # 결과 출력
-    if isEmpty(nums.items()):
-        print('EMPTY')
+    if min_heap and max_heap:
+        print(-max_heap[0][0], min_heap[0][0])
     else:
-        while min_heap[0] not in nums or nums[min_heap[0]] < 1:
-            heapq.heappop(min_heap)
-        while -max_heap[0] not in nums or nums[-max_heap[0]] < 1:
-            heapq.heappop(max_heap)
-        print(-max_heap[0], min_heap[0])
+        print('EMPTY')
