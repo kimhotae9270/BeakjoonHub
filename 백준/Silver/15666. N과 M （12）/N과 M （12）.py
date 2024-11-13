0d48e32a2f0b26e46238905e63503458ef6b5855
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-11-13 19:57:35 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -4,18 +4,17 @@ def back(lst):
     if len(lst) == m+1:
         print(*lst[1:])
         return
-    number = 0
-    for i in range(n):
-        if number != numLst[i] and lst[-1] <= numLst[i]:
 
+    for i in range(len(numLst)):
+        if lst[-1] <= numLst[i]:
             lst.append(numLst[i])
-            number = numLst[i]
             back(lst)
             lst.pop()
 
 
 n, m = map(int, sys.stdin.readline().split())
 numLst = list(map(int, sys.stdin.readline().split()))
+numLst = list(set(numLst))
 numLst.sort()
 
 back([0])
