2acaf3468da7db906bacd1a9b531323bb2a17652
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-11-17 18:35:54 +0900

    [Silver I] Title: 스티커, Time: 2404 ms, Memory: 51328 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,24 @@
+import sys
+
+chk = [[-1,-1],[-1,-2],[1,-1],[1,-2]]
+t = int(sys.stdin.readline())
+
+dp = []
+for i in range(t):
+    n = int(sys.stdin.readline())
+    dp = [[0 for _ in range(n)] for __ in range(2)]
+    lst = [[],[]]
+    for j in range(2):
+        lst[j] = list(map(int,sys.stdin.readline().split()))
+    dp[0][0] = lst[0][0]
+    dp[1][0] = lst[1][0]
+    for k in range(1,n):
+        for z in range(2):
+            for x,y in chk:
+                nx,ny = k+y,z+x
+                if 0<=ny<2 and 0<=nx<n:
+                    dp[z][k] = max(dp[z][k],dp[ny][nx]+lst[z][k])
+    maxi = 0
+    for l in dp:
+        maxi = max(maxi,max(l))
+    print(maxi)
