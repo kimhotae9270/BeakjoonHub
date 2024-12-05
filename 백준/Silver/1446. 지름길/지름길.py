902c316fd92269cf5e7530acb977b7cfec0010b3
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-12-05 21:10:46 +0900

    [Silver I] Title: 지름길, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,19 +1,18 @@
 import sys
 
-
-
 n,d = map(int,sys.stdin.readline().split())
-lst = [list(map(int, input().split())) for _ in range(n)]
-
+lst = []
 dp = [i for i in range(d+1)]
+for i in range(n):
+    start,end,dis = map(int, sys.stdin.readline().split())
+    if end > d:
+        continue
+    lst.append([start,end,dis])
 
 for i in range(d+1):
-    dp[i] = min(dp[i],dp[i-1]+1)
-    for s, e, short in lst:
-        if i == s and e <= d and dp[i]+short < dp[e]:
-            dp[e] = dp[i] + short
+    dp[i] = min(dp[i-1]+1,dp[i])
+    for j in range(len(lst)):
+        if i == lst[j][0]:
+            dp[lst[j][1]] = min(dp[lst[j][1]],lst[j][2]+dp[lst[j][0]])
 
 print(dp[d])
-
-
-
