6a895bfe89f0a8357548c5449624957df95cafdc
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-12-22 00:11:08 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+x, y = map(int, input().split())
+z = int(100 * y / x)
+
+start = 0
+end = x
+
+answer = -1
+while start <= end:
+    middle = (start + end) // 2
+    new_z = int(100 * (y + middle) / (x + middle))
+
+    if new_z > z:
+        answer = middle
+        end = middle - 1
+    else:
+        start = middle + 1
+
+print(answer)
