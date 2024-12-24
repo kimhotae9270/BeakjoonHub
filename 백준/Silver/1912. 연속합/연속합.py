eb57e585c415474595abd1bc560c94fc81e298ba
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2024-12-24 22:46:45 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline())
+lst = list(map(int,sys.stdin.readline().split()))
+dp = [0 for _ in range(n)]
+dp[0] = lst[0]
+for i in range(n):
+    dp[i] = max(lst[i],dp[i-1]+lst[i])
+
+print(max(dp))
