e77dfd3422cd41e6e41551be16dc06cc3f8f33fe
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2025-01-10 19:52:33 +0900

    [Gold V] Title: 줄어드는 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -0,0 +1,25 @@
+def dfs(end):
+    for i in range(end):
+        chk_lst.append(i)
+        dfs(i)
+        result.append(int("".join(map(str,chk_lst))))
+        chk_lst.pop()
+
+
+
+n = int(input())
+chk = [0,1,2,3,4,5,6,7,8,9]
+result = []
+if n > 1023:
+    print(-1)
+    exit()
+
+for i in chk:
+    chk_lst = []
+    chk_lst.append(i)
+    dfs(i)
+    result.append(int("".join(map(str,chk_lst))))
+    chk_lst.pop()
+result.sort()
+
+print(result[n-1])
