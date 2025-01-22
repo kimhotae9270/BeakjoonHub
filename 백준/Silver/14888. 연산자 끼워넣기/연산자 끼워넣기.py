407c21950112b3d8bd98f0dad5d942fe5be353ba
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2025-01-22 19:56:26 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 6576 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,9 +1,12 @@
 import sys
 
 def dfs(length):
+    global maxi
+    global mini
     global result
     if length == n:
-        result_lst.append(result)
+        maxi = max(maxi,result)
+        mini = min(mini,result)
         return
     for i in chk2:
         for j in range(dic[i]):
@@ -43,42 +46,13 @@ chk = list(map(int, sys.stdin.readline().split()))
 dic = {"+": chk[0], "-": chk[1], "*": chk[2], "/": chk[3]}
 chk2 = ["+", "-", "*", "/"]
 result = lst[0]
-result_lst = []
+mini = 1000000001
+maxi = -1000000001
 
-length = 1
-for i in chk2:
-    for j in range(dic[i]):
-        if i == "+":
-            result += lst[length]
-            dic[i] -= 1
-            dfs(length + 1)
-            result -= lst[length]
-            dic[i] += 1
-        elif i == "-":
-            result -= lst[length]
-            dic[i] -= 1
-            dfs(length + 1)
-            result += lst[length]
-            dic[i] += 1
-        elif i == "*":
-            result *= lst[length]
-            dic[i] -= 1
-            dfs(length + 1)
-            result //= lst[length]
-            dic[i] += 1
-        elif i == "/":
-            # C++14 기준의 음수 나눗셈 구현
-            prev_result = result
-            if result < 0:
-                result = -(-result // lst[length])
-            else:
-                result //= lst[length]
-            dic[i] -= 1
-            dfs(length + 1)
-            result = prev_result
-            dic[i] += 1
 
-result_lst.sort()
+dfs(1)
 
-print(max(result_lst))
-print((min(result_lst)))
+
+
+print(maxi)
+print(mini)
