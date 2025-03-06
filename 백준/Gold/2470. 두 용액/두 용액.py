7b3e699d5f4429f964411e30ff19403ef349c3fe
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2025-03-06 17:13:12 +0900

    [Gold V] Title: 두 용액, Time: 112 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,35 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+lst = list(map(int,sys.stdin.readline().split()))
+lst.sort()
+
+start = 0
+end = n - 1
+chk = 9876543210
+result = []
+while 1:
+    if start >= end:
+        break
+    val = lst[start] + lst[end]
+
+    if chk > abs(val):
+        chk = abs(val)
+        result = [lst[start], lst[end]]
+        if val > 0:
+            end -= 1
+        elif val < 0:
+            start += 1
+        elif val == 0:
+            break
+        continue
+    if val > 0:
+        end -= 1
+    elif val < 0:
+        start += 1
+
+
+
+result.sort()
+print(*result)
