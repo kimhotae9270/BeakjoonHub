a7024192d2a34ec60e975bf0d1c5b3cc52a56dc7
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2025-08-05 02:29:30 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,26 +1,26 @@
 import sys
 from collections import deque
+
 que = deque()
 n = int(sys.stdin.readline())
-for i in range(n):
-    lst = list(map(str,sys.stdin.readline().split()))
-    if len(lst) == 1:
-        cmd = lst[0]
-    else:
-        cmd, num = lst[0],int(lst[1])
+
+for _ in range(n):
+    lst = sys.stdin.readline().split()
+    cmd = lst[0]
+
     if cmd == "push_front":
-        que.appendleft(num)
+        que.appendleft(int(lst[1]))
     elif cmd == "push_back":
-        que.append(num)
+        que.append(int(lst[1]))
     elif cmd == "pop_front":
-        print(-1 if len(que) == 0 else que.popleft())
+        print(-1 if not que else que.popleft())
     elif cmd == "pop_back":
-        print(-1 if len(que) == 0 else que.pop())
+        print(-1 if not que else que.pop())
     elif cmd == "size":
         print(len(que))
     elif cmd == "empty":
-        print(1 if len(que) == 0 else 0)
+        print(1 if not que else 0)  # ✅ 수정됨
     elif cmd == "front":
-        print(-1 if len(que) == 0 else que[0])
+        print(-1 if not que else que[0])
     elif cmd == "back":
-        print(-1 if len(que) == 0 else que[-1])
+        print(-1 if not que else que[-1])
